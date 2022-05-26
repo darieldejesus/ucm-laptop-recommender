3d38d78e3865c12a2e5d8795c6f070fb7c70a6c7
Dariel de Jesus <darieldejesus@gmail.com>
2022-05-26 17:28:04 +0200

    Integrar reglas para capturar nombres

--- Templates/pln.py
+++ Templates/pln.py
@@ -6,7 +6,8 @@ from spacy.matcher import Matcher
 
 VERB_NOUN_PATTERN = [{"POS": "VERB"}, {"POS": "NOUN"}]
 NUM_PATTERN = [{"POS": "NUM"}]
-NAME_PATTERN = [{"POS": "PROPN"}, {"POS": "PROPN"}]
+FULL_NAME_PATTERN = [{"POS": "PROPN"}, {"POS": "ADP", "OP": "*"}, {"POS": "PROPN"}]
+NAME_PATTERN = [{"POS": "PROPN"}]
 
 def get_spacy():
     # return spacy.load('es_core_news_sm')
@@ -18,19 +19,41 @@ def extract_name(texto):
     res = {"found": False, "name":""}
 
     matcher = Matcher(nlp.vocab)
+    matcher.add("FULL_NAME", [FULL_NAME_PATTERN])
     matcher.add("NAME", [NAME_PATTERN])
 
     matches = matcher(doc)
     for match_id, start, end in matches:
         string_id = nlp.vocab.strings[match_id]
         span = doc[start:end]
-        if string_id == "NAME":
+        if string_id == "NAME" and res["name"] == "":
+            res = {
+                "found": True,
+                "name": span.text
+            }
+        if string_id == "FULL_NAME":
             res = {
                 "found": True,
                 "name": span.text
             }
 
     return res
+
+"""
+Funcion para convertir un texto a su base (lemma)
+Por ejemplo:
+- Entrada: "Ver Peliculas"
+- Salida: "Ver Pelicula"
+"""
+def extract_lemma(text):
+    nlp = get_spacy()
+    doc = nlp(text)
+
+    lemmas = []
+    for token in doc:
+        lemmas.append(token.lemma_)
+
+    return " ".join(lemmas)
 
 def extract_requirements(text):
     nlp = get_spacy()
@@ -48,7 +71,7 @@ def extract_requirements(text):
         string_id = nlp.vocab.strings[match_id]
         span = doc[start:end]
         if string_id == "VERB_NOUN":
-            categories.append(span.text)
+            categories.append(extract_lemma(span.text))
         if string_id == "NUMBER" and span.text.isdigit():
             budget = int(span.text)
 
